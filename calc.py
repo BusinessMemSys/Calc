09b5c8dea92e26b85373a0c6924ade97bb8376bd
BusinessMemSys <fyodere@mail.ru>
2021-06-17 21:20:34 +0600

    com2

--- calc.py
+++ calc.py
@@ -4,6 +4,8 @@ root.title('Calculator')
 root.geometry('320x450')
 root.resizable(False, False)
 b = []
+del_list = []
+arif_list = ['*',"/","+",'-']
 
 class Calculator():   
     def __init__(self, root):
@@ -31,29 +33,40 @@ class Calculator():
     def logical(self, a):
         if a == '^':
             b.append('**')
+        elif a =='Del':
+            del_list.append(a)
         else:
             b.append(a)
         k = ''.join(b)
         self.lbl.configure(text=k)   
         if a == '=':
-            l = k[0:-1]
-            res = eval(l)
-            b.clear()
-            b.append(str(res))
-            self.lbl.configure(text=res)   
+            print(b[0])
+            if b[0] in arif_list:
+                texxxt = "ERROR"
+                self.lbl.configure(text=texxxt)
+            else:
+                l = k[0:-1]
+                res = eval(l)
+                b.clear()
+                b.append(str(res))
+                self.lbl.configure(text=res)   
         elif a == 'C':
             b.clear()
             n = '0'
             self.lbl.configure(text=n)
         elif a == 'Del':
-            b.pop(len(b)-1)
-            b.pop(len(b)-1)
             if len(b) == 0:
-                self.lbl.configure(text='0')
-            elif len(b) == 1 and b[0] == 0:
-                self.lbl.configure(text='0')
+                n = '0'
+                self.lbl.configure(text=n)
+            elif len(b) == 1 and b[0] != '0':
+                b.clear()
+                n = '0'
+                self.lbl.configure(text=n)
+
             else:
+                b.pop(len(b)-1)
                 self.lbl.configure(text=(''.join(b)))  
+            del_list.clear()
 if __name__ == '__main__':
     app = Calculator(root)
     app.buttons()
